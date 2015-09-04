f6fbdac8d93b7f9386c5a160989730dc9417f4ae
Kyle <kylebasu@gmail.com>
2015-09-04 11:34:05 -0700

    CSS profile and editprofile

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -194,14 +194,16 @@ angular.module('starter.controllers', [])
         $scope.profilepic = snapshot.val().profilepicture[userId].profilepicture;
         //for(var i in $scope.interests)
         for(var i in $scope.interests){
-          activityObj[$scope.interests[i].activity] = 1
+          activityObj[$scope.interests[i].activity] = 1;
         }
-        for(var i = 0; i < activities.length; i++){
-          if(!activityObj[activities[i]]){
-            activitiesArr.push(activities[i]);
+        for(var k = 0; i < activities.length; i++){
+          if(!activityObj[activities[k]]){
+            activitiesArr.push(activities[k]);
           }
         }
         $scope.activities = activitiesArr;
+      }else{
+        $scope.activities = snapshot.val().activities;
       }
     });
   });
@@ -237,7 +239,7 @@ angular.module('starter.controllers', [])
         ref.child('interests').child(userId).push({
           'activity': item
         });
-  }
+  };
 
   $scope.addBio = function(item){
     ref.child('users').child(userId).update({
@@ -254,8 +256,8 @@ angular.module('starter.controllers', [])
         return;
       }
     }
-
-  }
+  };
+  
 
 })
 
@@ -279,12 +281,12 @@ angular.module('starter.controllers', [])
       return {
         'color': 'green',
         'text-align': 'right'
-      }
+      };
     } else {
       return {
         'color': 'red',
         'text-align': 'left'
-      }
+      };
     }
   };
 
