b7043bb34005c828583e7b1899c5ea47640d1f74
Alex Leonetti <leonetti.tech@gmail.com>
2015-09-16 11:08:21 -0700

    stashing changes

--- www/js/Controllers/events.group.controller.js
+++ www/js/Controllers/events.group.controller.js
@@ -4,6 +4,7 @@
     .controller('EventGroupCtrl', EventGroupCtrl);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     EventGroupCtrl.$inject = ['$scope', '$state', '$timeout', '$stateParams', '$ionicScrollDelegate', 'userService'];
 
     function EventGroupCtrl ($scope, $state, $timeout, $stateParams, $ionicScrollDelegate, userService) {
@@ -47,10 +48,52 @@
     }
 =======
     EventGroupCtrl.$inject = ['$scope', '$state', '$timeout'];
+=======
+    EventGroupCtrl.$inject = ['$scope', '$state', '$timeout', '$stateParams', '$ionicScrollDelegate', 'userService'];
+>>>>>>> stashing changes
 
-    function EventGroupCtrl ($scope, $state, $timeout) {
+    function EventGroupCtrl ($scope, $state, $timeout, $stateParams, $ionicScrollDelegate, userService) {
       var vm = this;
+      vm.text = '';
       vm.messages = [];
+<<<<<<< HEAD
     };
+>>>>>>> stashing changes
+=======
+      vm.eventId = $stateParams.eventId;
+      vm.photo;
+
+      ref.child('events').child(vm.eventId).child('messages').on('value', function(snapshot) {
+        vm.messages = [];
+        $timeout(function() {
+          for(var key in snapshot.val()) {
+            vm.messages.push(snapshot.val()[key]);
+          }
+        $ionicScrollDelegate.scrollBottom();
+        });
+      });
+
+      vm.sendMessage = function(message) {
+        if(message !== "") {
+          ref.child('users').child(window.localStorage['uid']).once('value', function(snapshot) {
+            var displayName = snapshot.val().displayName;
+            var photo;
+            ref.child('events').child(vm.eventId).child('messages').push({
+              sender: window.localStorage['uid'],
+              text: message,
+              date: Firebase.ServerValue.TIMESTAMP,
+              displayName: displayName,
+              photo: vm.photo
+            });
+          });
+        }
+        vm.text = '';
+        $ionicScrollDelegate.scrollBottom();
+      }
+
+      userService.getCompleteUser(window.localStorage['uid']).then(function(user){
+        vm.photo = user.profilepicture;
+      });
+    }
 >>>>>>> stashing changes
 })();
